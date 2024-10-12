35cadc181d7446f8705c544802627ebbef27477b
Anna <annamurray278@gmail.com>
2024-10-12 13:45:41 -0500

    pin types

--- include/pins.hpp
+++ include/pins.hpp
@@ -4,7 +4,7 @@
 // constexpr int pin = static_cast<int>(Pins::PIN_NAME);
 
 // change specific bounds after testing with sensors in pedalbox
-enum class Bounds {
+enum class Bounds : uint16_t {
     APPS1_MIN = 0,
     APPS1_MAX = 32767,
     APPS2_MIN = 0,
@@ -12,11 +12,12 @@ enum class Bounds {
     FRONT_BRAKE_MIN = 0,
     FRONT_BRAKE_MAX = 32767,
     REAR_BRAKE_MIN = 0,
-    REAR_BRAKE_MAX = 32767
+    REAR_BRAKE_MAX = 32767,
+    BRAKE_PRESSED_THRESHOLD = 1000 // actual threshold is TBD, need to test with brake sensors
 };
 
 // use GPIO names
-enum class Pins {
+enum class Pins : uint8_t {
     APPS1_CS_PIN = 0,
     APPS2_CS_PIN = 0,
     FRONT_BRAKE_CS_PIN = 0,
