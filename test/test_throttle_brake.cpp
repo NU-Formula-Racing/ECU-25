8644032f0c694f080d2c4eb2534b390d4fffa31e
Jeremy <jeremy9k27@gmail.com>
2024-11-06 20:23:16 -0600

    first unit test

---
+++ test/test_throttle_brake.cpp
@@ -0,0 +1,33 @@
+#include <unity.h>
+#include "throttle_brake_driver.hpp"
+#include "esp_can.h"
+
+// instantiate CAN bus
+ESPCAN drive_bus{};
+
+
+void setUp(void) {
+
+}
+
+void tearDown(void) {
+
+}
+
+void testIsBreakPressed() {
+    ThrottleBrake throttle_brake(drive_bus, 1001);
+    TEST_ASSERT_EQUAL(throttle_brake.is_brake_pressed(), true);
+
+    ThrottleBrake throttle_brake(drive_bus, 999);
+    TEST_ASSERT_EQUAL(throttle_brake.is_brake_pressed(), false);
+
+}
+
+int main(int argc, char **argv) {
+    UNITY_BEGIN();
+
+    RUN_TEST(testIsBreakPressed);
+
+    return UNITY_END();
+
+}
