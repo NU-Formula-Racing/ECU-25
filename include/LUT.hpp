01ddb21ef6391a0127e8ba72d2099660d2e58af7
Anna <annamurray278@gmail.com>
2025-03-10 19:26:22 -0500

    init

--- include/LUT.hpp
+++ include/LUT.hpp
@@ -4,16 +4,16 @@
 #include <map>
 
 // IGBT temp : milliAmp
-extern std::map<uint16_t, int16_t> IGBTtemp2mAmp_LUT;
+extern std::map<uint16_t, float> IGBTtemp2mAmp_LUT;
 
 // Battery temp : milliAmp
-extern std::map<uint16_t, int16_t> batterytemp2mAmp_LUT;
+extern std::map<uint16_t, float> batterytemp2mAmp_LUT;
 
 // Motor temp : milliAmp
-extern std::map<uint16_t, int16_t> motortemp2mAmp_LUT;
+extern std::map<uint16_t, float> motortemp2mAmp_LUT;
 
 // Motor RPM : milliAmp
-extern std::map<uint16_t, int16_t> motorRPM2mAmp_LUT;
+extern std::map<uint16_t, float> motorRPM2mAmp_LUT;
 
 // Throttle % : float (when accelerating: factor to multiply by min(other LUTS) -- this product is
 // the torque requested)
--- src/LUT.cpp
+++ src/LUT.cpp
@@ -2,25 +2,25 @@
 #include <map>
 
 // IGBT temp : milliAmp
-std::map<uint16_t, int16_t> IGBTtemp2mAmp_LUT{
+std::map<uint16_t, float> IGBTtemp2mAmp_LUT{
     {0, 0}, {1, 1},
     // get LUT values from Benji sims
 };
 
 // Battery temp : milliAmp
-std::map<uint16_t, int16_t> batterytemp2mAmp_LUT{
+std::map<uint16_t, float> batterytemp2mAmp_LUT{
     {0, 0}, {1, 1},
     // get LUT values from Benji sims
 };
 
 // Motor temp : milliAmp
-std::map<uint16_t, int16_t> motortemp2mAmp_LUT{
+std::map<uint16_t, float> motortemp2mAmp_LUT{
     {0, 0}, {1, 1},
     // get LUT values from Benji sims
 };
 
 // Motor RPM : milliAmp
-std::map<uint16_t, int16_t> motorRPM2mAmp_LUT{
+std::map<uint16_t, float> motorRPM2mAmp_LUT{
     {0, 0}, {1, 1},
     // get LUT values from Benji sims
 };
